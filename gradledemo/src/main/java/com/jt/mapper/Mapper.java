65563e12e12ad3f1d79c16ac45f89b2e86c8f4c6
huanghuan123456 <huanghuan123my@163.com>
2021-10-07 16:22:35 +0800

    测试

--- gradledemo/src/main/java/com/jt/mapper/Mapper.java
+++ gradledemo/src/main/java/com/jt/mapper/Mapper.java
@@ -3,21 +3,6 @@ package com.jt.mapper;
 public class Mapper {
     public static void main(String[] args) {
         System.out.println("asdad");
-        System.out.println("a1");
-        System.out.println("b1");
-        System.out.println("b1");
-        System.out.println("b2");
-        System.out.println("b3");
-        System.out.println("b4");
-        System.out.println("b5");
-        System.out.println("b6");
-        System.out.println("1");
-        System.out.println("b6");
-        System.out.println("b6");
-        System.out.println("b6");
-        System.out.println("b6");
-        System.out.println("b6");
-        System.out.println("b6");
-        System.out.println("b6");
+
     }
 }
