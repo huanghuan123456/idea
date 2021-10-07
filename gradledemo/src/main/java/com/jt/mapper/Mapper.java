7cac92c409c97057bf115a4a56b2b8527dd47935
huanghuan123456 <huanghuan123my@163.com>
2021-10-07 15:30:08 +0800

    测试

--- gradledemo/src/main/java/com/jt/mapper/Mapper.java
+++ gradledemo/src/main/java/com/jt/mapper/Mapper.java
@@ -16,7 +16,5 @@ public class Mapper {
         System.out.println("sdada");
         System.out.println("1245");
         System.out.println("321");
-        System.out.println("31");
-        System.out.println("12");
     }
 }
