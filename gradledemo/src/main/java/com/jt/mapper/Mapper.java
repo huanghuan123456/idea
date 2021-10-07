4b8725ccbad7777e58c07f129d26ff3fa5717e3b
huanghuan123456 <huanghuan123my@163.com>
2021-10-07 11:42:17 +0800

    测试

--- gradledemo/src/main/java/com/jt/mapper/Mapper.java
+++ gradledemo/src/main/java/com/jt/mapper/Mapper.java
@@ -4,5 +4,7 @@ public class Mapper {
     public static void main(String[] args) {
         System.out.println("asdad");
         System.out.println("asdadad");
+        System.out.println("uadhuad");
+        System.out.println("sdadad");
     }
 }
